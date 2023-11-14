1bc915a11bb30df4f7192b4008dad216061f34b2
SandWithCheese <naufalahmad022@gmail.com>
2023-11-14 22:57:57 +0700

    feat: integrate color algorithm to website

--- src/cbir_web/pages/views.py
+++ src/cbir_web/pages/views.py
@@ -62,6 +62,92 @@ def component(matrix):
     return [contrast, homogeneity, entropy]
 
 
+# Mengubah data RGB dari suatu image menjadi data histogram H, S, dan V.
+# Menerima masukan berupa image dengan format array of RGB
+# Menghasilkan keluaran berupa histogram H, S, dan V
+def rgb_to_hsv_hist(rgb_image):
+    # Normalize RGB values to the range [0, 1]
+    normalized_image = rgb_image / 255.0
+
+    # Find Cmax, Cmin, and ∆
+    Cmax = np.max(normalized_image, axis=-1)
+    Cmin = np.min(normalized_image, axis=-1)
+    delta = Cmax - Cmin
+
+    # Calculate Hue (H)
+    H = np.zeros_like(Cmax)
+    non_zero_delta = delta != 0
+
+    H[non_zero_delta] = np.where(
+        Cmax[non_zero_delta] == normalized_image[non_zero_delta, ..., 0],
+        (
+            (
+                (
+                    normalized_image[non_zero_delta, ..., 1]
+                    - normalized_image[non_zero_delta, ..., 2]
+                )
+                / delta[non_zero_delta]
+            )
+            % 6.0
+        ),
+        H[non_zero_delta],
+    )
+
+    H[non_zero_delta] = np.where(
+        Cmax[non_zero_delta] == normalized_image[non_zero_delta, ..., 1],
+        (
+            2.0
+            + (
+                normalized_image[non_zero_delta, ..., 2]
+                - normalized_image[non_zero_delta, ..., 0]
+            )
+            / delta[non_zero_delta]
+        ),
+        H[non_zero_delta],
+    )
+
+    H[non_zero_delta] = np.where(
+        Cmax[non_zero_delta] == normalized_image[non_zero_delta, ..., 2],
+        (
+            4.0
+            + (
+                normalized_image[non_zero_delta, ..., 0]
+                - normalized_image[non_zero_delta, ..., 1]
+            )
+            / delta[non_zero_delta]
+        ),
+        H[non_zero_delta],
+    )
+
+    H = (H / 6.0) % 1.0
+
+    # Calculate Saturation (S)
+    S = np.zeros_like(Cmax)
+    S[Cmax != 0] = delta[Cmax != 0] / Cmax[Cmax != 0]
+
+    # Calculate Value (V)
+    V = Cmax
+
+    hist_h = np.histogram(
+        H,
+        bins=[
+            0,
+            25 / 360,
+            40 / 360,
+            120 / 360,
+            190 / 360,
+            270 / 360,
+            295 / 360,
+            315 / 360,
+            360 / 360,
+        ],
+    )
+    hist_s = np.histogram(S, bins=[0, 0.2, 0.7, 1])
+    hist_v = np.histogram(V, bins=[0, 0.2, 0.7, 1])
+
+    return hist_h, hist_s, hist_v
+
+
 # Create your views here.
 def index(request):
     files = Dataset.objects.all()
@@ -93,7 +179,97 @@ def index(request):
 
                 json_dir = os.listdir(JSON_ROOT)
 
-                if request.POST["cbir_mode"] == "texture":
+                if request.POST["cbir_mode"] == "color":
+                    start = time.time()
+
+                    imgA = Image.open(MEDIA_ROOT + "test.jpg")
+                    test_h, test_s, test_v = rgb_to_hsv_hist(np.array(imgA))
+
+                    if "color.json" not in json_dir:
+                        list_files = os.listdir(MEDIA_ROOT + "dataset/")
+                        for f in list_files:
+                            imgB = Image.open(MEDIA_ROOT + "dataset/" + f)
+                            data_h, data_s, data_v = rgb_to_hsv_hist(np.array(imgB))
+
+                            similarity_h = cosine_similarity(test_h[0], data_h[0])
+                            similarity_s = cosine_similarity(test_s[0], data_s[0])
+                            similarity_v = cosine_similarity(test_v[0], data_v[0])
+
+                            avg_similarity = (
+                                similarity_h + similarity_s + similarity_v
+                            ) / 3
+
+                            avg_similarity = float(str(avg_similarity)[:5])
+
+                            if avg_similarity > 60:
+                                query.append(
+                                    {
+                                        "file": "media/dataset/" + f,
+                                        "hist_h": data_h[0].tolist(),
+                                        "hist_s": data_s[0].tolist(),
+                                        "hist_v": data_v[0].tolist(),
+                                        "percentage": avg_similarity,
+                                    }
+                                )
+
+                        with open(JSON_ROOT + "color.json", "w") as f:
+                            json.dump(query, f)
+                    else:
+                        with open(JSON_ROOT + "color.json", "r") as f:
+                            data = json.load(f)
+                            for d in data:
+                                data_h = d["hist_h"]
+                                data_s = d["hist_s"]
+                                data_v = d["hist_v"]
+
+                                similarity_h = cosine_similarity(test_h[0], data_h)
+                                similarity_s = cosine_similarity(test_s[0], data_s)
+                                similarity_v = cosine_similarity(test_v[0], data_v)
+
+                                avg_similarity = (
+                                    similarity_h + similarity_s + similarity_v
+                                ) / 3
+
+                                avg_similarity = float(str(avg_similarity)[:5])
+
+                                if avg_similarity > 60:
+                                    query.append(
+                                        {
+                                            "file": d["file"],
+                                            "percentage": avg_similarity,
+                                        }
+                                    )
+
+                    result = list(
+                        sorted(query, key=lambda i: i["percentage"], reverse=True)
+                    )
+
+                    with open(JSON_ROOT + "result.json", "w") as f:
+                        json.dump(result, f)
+
+                    paginator = Paginator(result, 6)
+                    page_number = request.GET.get("page")
+                    page_obj = paginator.get_page(page_number)
+
+                    end = time.time()
+
+                    time_taken = round((end - start), 2)
+
+                    with open(JSON_ROOT + "time.json", "w") as f:
+                        json.dump(time_taken, f)
+
+                    return render(
+                        request,
+                        "pages/index.html",
+                        context={
+                            "form": form,
+                            "dataset_form": dataset_form,
+                            "image": "media/test.jpg",
+                            "page_obj": page_obj,
+                            "time_taken": time_taken,
+                        },
+                    )
+                elif request.POST["cbir_mode"] == "texture":
                     start = time.time()
 
                     vectorA = component(
--- test/README.md
+++
@@ -1,3 +0,0 @@
-# Test
-
-Gambar untuk pengujian
